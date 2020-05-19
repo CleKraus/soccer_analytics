f71c611016181544f6b5856c9412e7e31dbbf684
Kraus Clemens <kraus.clemens@bcg.com>
2020-05-19 15:01:18 +0200

    Rename folder

--- preprocessing/cleansing_wyscout_data/wyscout_cleansing_pipeline.py
+++ preprocessing/cleansing_wyscout_data/wyscout_cleansing_pipeline.py
@@ -3,10 +3,10 @@
 # import packages
 import logging
 
-from preprocessing.cleaning_wyscout_data.event_data import cleanse_wyscout_event_data
-from preprocessing.cleaning_wyscout_data.match_data import cleanse_wyscout_match_data
-from preprocessing.cleaning_wyscout_data.player_data import cleanse_wyscout_player_data
-from preprocessing.cleaning_wyscout_data.team_data import cleanse_wyscout_team_data
+from preprocessing.cleansing_wyscout_data.event_data import cleanse_wyscout_event_data
+from preprocessing.cleansing_wyscout_data.match_data import cleanse_wyscout_match_data
+from preprocessing.cleansing_wyscout_data.player_data import cleanse_wyscout_player_data
+from preprocessing.cleansing_wyscout_data.team_data import cleanse_wyscout_team_data
 
 logging.basicConfig(level=logging.DEBUG)
 
